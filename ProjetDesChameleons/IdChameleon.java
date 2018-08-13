2ccada498384218ae50d4827b77089ba2162053d
Salam Rizk <25846376+salamrizk1@users.noreply.github.com>
2018-08-13 10:08:40 +0300

    Add files via upload

---
+++ ProjetDesChameleons/IdChameleon.java
@@ -0,0 +1,10 @@
+package ProjetDesChameleons;
+
+public class IdChameleon{
+
+	private final int valeur ;
+	public IdChameleon(int val) {
+		valeur = val; }
+	public String toString () {
+		return valeur + ””; }
+}
